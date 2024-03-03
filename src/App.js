6f002b6ee42c6ed83d357a842038a2e9a0e76cd1
sunny kumar <sunnykumar2020has@gmail.com>
2024-03-03 09:26:56 +0530

    changes saved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
-
+// also used as the global variable
+const style="App-header";
 function App() {
+  // used as the local variable that is valid for the current components.
+  const topic = "Hello Developer, Welcome to ReactJs!";
+
+  return (
+    <div>
+      <div className={style}> {topic}
+        <Demo></Demo>
+      </div>
+
+    </div>
+  );
+}
+
+
+function Demo() {
   return (
     <div className="App">
-        <div className="App-header">Hello Developer, Welcome to ReactJs!</div>
+        <div className={style}>Now Start reactJs</div>
     </div>
   );
 }
