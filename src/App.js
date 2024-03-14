39809341bbe1cdf5ffe3d5e33148b064abc4e624
sunny kumar <sunnykumar2020has@gmail.com>
2024-03-15 00:17:24 +0530

    changes saved

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './App.css';
 // import multiple components.
 // import {Video,Thumb} from './components/Video';
-
 import Video from './components/Video';
 
 const App = () => {
+  // creating a object that would be used as a for component as destructured of object.
   const channel = "SUNNY KUMAR";
   let obj = {
     title: "ReactJs Tutorial",
@@ -17,7 +17,7 @@ const App = () => {
   return (
     <div className='App'>
       <div>Videos</div>
-      
+
       {/* spread operator */}
       <Video {...obj} ></Video>
       <Video title="NodeJs Tutorial" views="10k" time="1 month ago" channel={channel} ></Video>
--- src/components/Video.js
+++ src/components/Video.js
@@ -6,7 +6,7 @@ import './Video.css';
 const Video = ({ title, channel = "SUNNY", views, time }) => {
     // let topic = "ReactJs Tutorial - ";
     // console.log(props);
-
+    // props is read only that can not be changed on run time.
     //let bg = 'dark'; // this will be ignored beaucse you have already uses the css inline the code.
     // bgColor = 'blue'; // we can change the coming value of bgColor and that will reflect on the screen.
 
@@ -16,7 +16,6 @@ const Video = ({ title, channel = "SUNNY", views, time }) => {
                 <div>
                     <img src="https://placebeard.it/160x90" alt="Image_placeholder" />
                 </div>
-
                 <div className="title" >{title}</div>
                 <div className="channel" >{channel}</div>
                 <div className="views" >{views} Views<span>.</span>{time}</div>
@@ -26,9 +25,6 @@ const Video = ({ title, channel = "SUNNY", views, time }) => {
     )
 }
 
-
-
-
 // const Thumb = () => {
 //   return (
 //     <div>ThumbNail</div>
@@ -36,4 +32,6 @@ const Video = ({ title, channel = "SUNNY", views, time }) => {
 // }
 
 // export multiple component.
+// if you make more than one component in the one file export them as object shorthand and import them as also in the object destructured way.
+// but this us not the good way you should make the different component in the different file and export and import them differently.
 export default Video;
