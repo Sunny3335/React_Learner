47f25951d6f13e249685d08bc03e0d79fc30f149
sunny kumar <sunnykumar2020has@gmail.com>
2024-03-18 18:14:20 +0530

    changes saved

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import Video from './components/Video';
 import Videos from './data/data';
 const App = () => {
 
-  /* here we put {} to write the js code */
+    /* here we put {} to write the js code */
 
-  return (
-    <div className='App'>
-      <div>Videos</div>
-      {Videos.map(video =>
-        <Video
-          key={video.id}
-          verified={video.verified}
-          title={video.title}
-          views={video.views}
-          time={video.time}
-          id={video.id}
-        ></Video>)}
+    return (
+        <div className='App'>
+            <div>Videos</div>
+            {Videos.map(video =>
+                <Video
+                    key={video.id}
+                    verified={video.verified}
+                    title={video.title}
+                    views={video.views}
+                    time={video.time}
+                    id={video.id}
+                ></Video>)}
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default App;
