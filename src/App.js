fa92af051bdb1dec0ef9f9842a982ceb1868b8a2
sunny kumar <sunnykumar2020has@gmail.com>
2024-03-18 18:20:14 +0530

    changes saved

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css'; // also used as the global variable.
 const style = "App-header";
 function App() {
-  const topic = "Hello Developer, Welcome to ReactJs!"; // used as the local variable that is valid for the current components.
-  return (
-    <>
-      <div className={style}>{topic}
-        <Demo/>
-      </div>
-    </>
-  );
+    const topic = "Hello Developer, Welcome to ReactJs!"; // used as the local variable that is valid for the current components.
+    return (
+        <>
+            <div className={style}>{topic}
+                <Demo />
+            </div>
+        </>
+    );
 }
 
 function Demo() {
-  return (
-    <>
-      <div className={style}>Now Start reactJs</div>
-    </>
-  );
+    return (
+        <>
+            <div className={style}>Now Start reactJs</div>
+        </>
+    );
 }
 
 export default App; // Here I have maked two function and link them in the exported one file and export as App.js and also import these as App.js
